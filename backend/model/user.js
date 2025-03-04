c4d615c6befe25379787942efa59c0c5053d04b5
Rahul2006019 <vigneshrahulr@gmail.com>
2025-03-04 12:04:02 +0530

    updated

--- backend/model/user.js
+++ backend/model/user.js
@@ -57,15 +57,28 @@ const userSchema = new mongoose.Schema({
       // required: true,
     },
  },
- createdAt:{
-  type: Date,
-  default: Date.now(),
- },
- resetPasswordToken: String,
- resetPasswordTime: Date,
+ cart: [
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [1, "Quantity cannot be less than 1"],
+      default: 1,
+    },
+  },
+],
+createdAt:{
+type: Date,
+default: Date.now(),
+},
+resetPasswordToken: String,
+resetPasswordTime: Date,
 });
-
-
 
 
 //  Hash password
